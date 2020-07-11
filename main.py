f39ff0d338b12d8b30554adf5f84d014c4b2f356
Bartosz Szadkowski <bartoszszadkowski@MacBook-Air-Bartosz.local>
2020-07-11 20:00:26 +0200

    actually program works fine but it could works better, Next step to make: one single element of the printable array is a character set, not single character. By default, one element has min 4 characters

--- main.py
+++ main.py
@@ -37,9 +37,44 @@ while run:
     with open(file.path, 'rb') as fileText:
         fileText = fileText.read()
     print(fileText)
-
-    # creating regex object to find printable sets of signs
-    charSetReg = re.compile()
     fileText = str(fileText)
     print(fileText)
-    print(charSetReg.findall(fileText))
+
+    fileText = fileText[2:len(fileText)-1]
+
+    print(fileText)
+
+    #this shit from the top doesn't work, so it should be an algorithm in loop
+
+    bytesErase = False
+
+    printable = ''
+
+    while not bytesErase:
+        if fileText[0] == '\\' and fileText[1] == 'x':
+            fileText = fileText[4:]
+        elif fileText[0] == '\\' and (fileText[1] == 'n' or fileText[1] == 't'):
+            fileText = fileText[2:]
+        else:
+            printable += fileText[0]
+            fileText = fileText[1:]
+
+        if len(fileText) == 0:
+            bytesErase = True
+
+    print(printable)
+    counter = 0
+    row = ''
+    for i in range(len(printable)):
+        if counter == charSetLength:
+            print(row)
+            #print('\n')
+            row = ''
+            counter = 0
+        row += printable[i]
+        counter += 1
+
+
+
+
+
